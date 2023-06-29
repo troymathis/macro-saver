34b8442758be92c2fa8e5adcb28b890d619abe99
Troy Mathis <troy.mathis00@gmail.com>
2023-06-29 15:00:28 -0500

    some components, working on navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { getServerSession } from "next-auth";
+import { FC } from "react";
+import Link from "next/link";
+import ThemeToggle from "@/ui/ThemeToggle";
+import { buttonVariants } from "@/ui/Button";
+
+interface NavbarProps {}
+
+const Navbar = async ({}) => {
+  const session = await getServerSession();
+  return (
+    <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
+      <Link href="/" className={buttonVariants({ variant: "link" })}>
+        MacroSaver
+      </Link>
+      <div className="md:hidden">
+          <ThemeToggle />
+        </div>
+        <div className="hidden md:flex gap-4">
+          <ThemeToggle />
+          {session ? (
+            <>
+              <Link
+                className={buttonVariants({ variant: "ghost" })}
+                href="/dashboard"
+              >
+                Dashboard
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+    </div>
+  );
+};
+
+export default Navbar;
